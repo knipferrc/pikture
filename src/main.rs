6559761684065b74668dfe0c7995172bac701397
knipferrc <knipferrc@gmail.com>
2023-04-02 15:41:50 -0400

    feat: switch to relm4

--- src/main.rs
+++ src/main.rs
@@ -1,85 +1,80 @@
-#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
+use gtk::prelude::{BoxExt, ButtonExt, GtkWindowExt, OrientableExt};
+use relm4::{adw, gtk, ComponentParts, ComponentSender, RelmApp, RelmWidgetExt, SimpleComponent};
 
-use eframe::egui;
-use egui_extras::RetainedImage;
-use std::fs;
-
-fn main() -> Result<(), eframe::Error> {
-    let options = eframe::NativeOptions {
-        initial_window_size: Some(egui::vec2(320.0, 240.0)),
-        ..Default::default()
-    };
-    eframe::run_native(
-        "Refile",
-        options,
-        Box::new(|_cc| Box::new(Refile::default())),
-    )
+struct AppModel {
+    counter: u8,
 }
 
-struct Refile {
-    selected_directory: String,
-    folder_icon: RetainedImage,
-    file_icon: RetainedImage,
+#[derive(Debug)]
+enum AppMsg {
+    Increment,
+    Decrement,
 }
 
-impl Default for Refile {
-    fn default() -> Self {
-        Self {
-            selected_directory: String::from("."),
-            folder_icon: RetainedImage::from_image_bytes(
-                "folder.png",
-                include_bytes!("folder.png"),
-            )
-            .unwrap(),
-            file_icon: RetainedImage::from_image_bytes("file.png", include_bytes!("file.png"))
-                .unwrap(),
+#[relm4::component]
+impl SimpleComponent for AppModel {
+    type Init = u8;
+
+    type Input = AppMsg;
+    type Output = ();
+
+    view! {
+        adw::Window {
+            set_title: Some("Refile"),
+
+            gtk::Box {
+                set_orientation: gtk::Orientation::Vertical,
+                set_spacing: 5,
+                set_margin_all: 5,
+
+                gtk::Button {
+                    set_label: "Increment",
+                    connect_clicked[sender] => move |_| {
+                        sender.input(AppMsg::Increment);
+                    }
+                },
+
+                gtk::Button {
+                    set_label: "Decrement",
+                    connect_clicked[sender] => move |_| {
+                        sender.input(AppMsg::Decrement);
+                    }
+                },
+
+                gtk::Label {
+                    #[watch]
+                    set_label: &format!("Counter: {}", model.counter),
+                    set_margin_all: 5,
+                }
+            }
+        }
+    }
+
+    fn init(
+        counter: Self::Init,
+        root: &Self::Root,
+        sender: ComponentSender<Self>,
+    ) -> ComponentParts<Self> {
+        let model = AppModel { counter };
+
+        let widgets = view_output!();
+
+        ComponentParts { model, widgets }
+    }
+
+    fn update(&mut self, msg: Self::Input, _sender: ComponentSender<Self>) {
+        match msg {
+            AppMsg::Increment => {
+                self.counter = self.counter.wrapping_add(1);
+            }
+            AppMsg::Decrement => {
+                self.counter = self.counter.wrapping_sub(1);
+            }
         }
     }
 }
 
-impl eframe::App for Refile {
-    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        egui::SidePanel::left("folder_panel").show(ctx, |ui| {
-            if ui.button(format!("{}", "Home")).clicked() {
-                self.selected_directory = ".".to_string();
-            }
-            if ui.button(format!("{}", "Root")).clicked() {
-                self.selected_directory = "/".to_string();
-            }
-            if ui.button(format!("{}", "Home")).clicked() {
-                self.selected_directory = "/Downloads".to_string();
-            }
-        });
-
-        egui::CentralPanel::default().show(ctx, |ui| {
-            ui.horizontal_wrapped(|ui| {
-                for file in fs::read_dir(&self.selected_directory).unwrap() {
-                    let file = file.unwrap();
-                    let current_path = file.path().display().to_string();
-                    if file.path().is_dir() {
-                        ui.vertical(|ui| {
-                            if ui
-                                .add(egui::ImageButton::new(
-                                    self.folder_icon.texture_id(ctx),
-                                    self.folder_icon.size_vec2(),
-                                ))
-                                .double_clicked()
-                            {
-                                self.selected_directory = current_path;
-                            }
-                            ui.heading("Folder")
-                        });
-                    } else {
-                        ui.vertical(|ui| {
-                            ui.add(egui::ImageButton::new(
-                                self.file_icon.texture_id(ctx),
-                                self.file_icon.size_vec2(),
-                            ));
-                            ui.heading("File")
-                        });
-                    }
-                }
-            });
-        });
-    }
+fn main() {
+    let app = RelmApp::new("org.mistakenelf.refile");
+    app.run::<AppModel>(0);
 }
